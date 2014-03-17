de19168845ce46f0433d35ad398bc8285cbeebf5
Kyle Geske <stungeye@gmail.com>
2014-03-17 09:09:25 -0500

    Initial commit of an empty Rails projecct.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleStore91461::Application.config.secret_key_base = '035c205443c2ab8c90d63dd37f9bd8cde7010195c563941b76a253bc42457e0f11600a5ea3f69aee626f937b3c29f606c22161414671933a80fd929407fe6499'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleStore91461::Application.config.session_store :cookie_store, key: '_simple_store_91461_session'
